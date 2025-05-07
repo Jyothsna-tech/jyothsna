2b9ceb70498d91b6fde758c34d76f6bbb11f8cad
Jyothsna-tech <jyothsnasingadi217@gmail.com>
2025-05-07 11:41:56 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+ //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+ class addition {
+  public static void main(String[] args) {
+   int a,b,c,add;
+   a=6;
+   b=7;
+   c=8;
+   add=a+b+c;
+   System.out.println("add="+add);
+  }
+ }
+
+
---
+++ src/Mul.java
@@ -0,0 +1,13 @@
+
+
+class Mul {
+    public static void main(String[] args) {
+        int a,b,c,Mul;
+        a=6;
+        b=7;
+        c=8;
+        Mul=a*b*c;
+        System.out.println("Mul="+Mul);
+    }
+}
+
