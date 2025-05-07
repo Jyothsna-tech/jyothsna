d03c9ef3807a7c3170b66e40935d2844fd8f797d
Jyothsna-tech <jyothsnasingadi217@gmail.com>
2025-05-07 17:12:58 +0530

    DIvision class

---
+++ src/Division.java
@@ -0,0 +1,14 @@
+	class division{
+    public static void main(String args[])
+    {
+        int a,b,qdiv,rdiv;
+        a=10;
+        b=4;
+        qdiv=a/b;
+        rdiv=a%b;
+        System.out.println("qdiv="+qdiv);
+        System.out.println("rdiv="+rdiv);
+    }
+}
+
+
